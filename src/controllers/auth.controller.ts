d7bc406ea8aea4b5bd00385380f14f781bb5fe3f
Luis Mera <luismera86@gmail.com>
2023-12-31 00:29:51 -0300

    login funcionando y agregado token

--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -2,30 +2,33 @@ import { NextFunction, Request, Response } from "express";
 import { UserService } from "../services/user.service";
 import { comparePassword } from "../utils/haspassword";
 import { ErrorHandled } from "../utils/errorHandled";
-import { generateToken } from "../utils/jwt";
+import { generateToken, verifyToken } from "../utils/jwt";
+
 
 export class AuthController {
   private userService: UserService = new UserService();
   login = async (req: Request, res: Response, next: NextFunction) => {
-    
+    const { email, password } = req.body;
     try {
-      console.log("login");
-      console.log(req.body);
+      const user = await this.userService.getOneByEmail(email);
 
-      // const user = await this.userService.getOneByEmail(email);
+      const checkPassword = comparePassword(password, user.password);
 
-      // const checkPassword = comparePassword(password, user.password);
+      const token = generateToken({ id: user.id, email: user.email });
 
-      // const token = generateToken({ id: user.id, email: user.email });
+      if (!checkPassword) throw ErrorHandled.errorBadRequest("Password incorrect");
 
-      // if (!checkPassword) throw ErrorHandled.errorBadRequest("Password incorrect");
+      res.cookie("user", token, { maxAge: 3600000, httpOnly: true, secure: true, sameSite: "strict" });
 
-      // res.cookie("user", token, { maxAge: 3600000, httpOnly: true, secure: true, sameSite: "strict" })
+      const cookies = req.cookies.user;
 
-      // res.status(200).json({
-      //   status: "success",
-      //   payload: token
-      // });
+      const decoded = verifyToken(cookies);
+      console.log(decoded);
+
+      res.status(200).json({
+        status: "success",
+        payload: token,
+      });
     } catch (error) {
       next(error);
     }
